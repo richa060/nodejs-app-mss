fbb5de33b8861f927f9675b8d863332654f752a1
richa060 <100208722+richa060@users.noreply.github.com>
2022-05-03 18:45:21 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.66.240.215:9000/',
+  serverUrl: 'http://65.2.75.149:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
